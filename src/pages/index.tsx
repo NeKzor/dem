9ef2302f2de1b4feecbdd8d458a1e928c6cba329
NeKz <nekzor@users.noreply.github.com>
2023-09-27 06:54:14 +0200

    Fix description meta tag

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,7 @@ export default function Home(): JSX.Element {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description="Documentation of the .dem file format.">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
